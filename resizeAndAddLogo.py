cb693f461a8d77e76587768e3f7ea95fe9ec8798
Phillip Vo <pvo4@mail.valenciacollege.edu>
2016-04-18 17:38:22 -0400

    Attempt 6

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -29,7 +29,7 @@ for filename in os.listdir('.'):
 
         #Resize the image
         print('Resizing %s...' % (filename))
-    im = im.resize((width, height))      
+        im = im.resize((width, height))      
 
     #TODO: Add the logo.
     print('Adding logo to %s...' % (filename))
